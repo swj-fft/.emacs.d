a4256ea9445a679da476244bd53f3d1b02ecc2e9
Wenjie Song <swj@fft.fund>
2025-08-14 16:07:26 +0800

    * init.el: Update config.

--- init.el
+++ init.el
@@ -60,41 +60,6 @@
   :config
   (setq doom-modeline-buffer-file-name-style 'truncate-upto-project)) ; Minimize buffer name display
 
-;; Native tab-line-mode for buffer tabs
-;; (use-package tab-line
-;;   :ensure nil
-;;   :config
-;;   (global-tab-line-mode 1)
-;;   (setq tab-line-background-color "#282c34"
-;;         tab-line-separator " | "
-;;         tab-line-close-button-show nil
-;;         tab-line-new-button-show nil
-;;         tab-line-tab-name-function #'tab-line-tab-name-buffer) ; Show only buffer name
-;;   (defun my-tab-line-buffer-groups ()
-;;     "Group buffers for tab-line."
-;;     (list
-;;      (cond
-;;       ((string-match-p "^\\*.*\\*$" (buffer-name)) "Emacs")
-;;       ((eq major-mode 'dired-mode) "Dired")
-;;       (t "User"))))
-;;   (setq tab-line-tabs-buffer-group-function #'my-tab-line-buffer-groups)
-;;   :bind
-;;   (("C-<tab>" . tab-line-switch-to-next-tab)
-;;    ("C-S-<tab>" . tab-line-switch-to-prev-tab)))
-
-;; Native tab-bar-mode for workspace tabs (optional, disable if not needed)
-(use-package tab-bar
-  :ensure nil
-  :config
-  (tab-bar-mode 1) ; Set to -1 to disable if causing duplication
-  (setq tab-bar-show t
-        tab-bar-new-tab-to 'right
-        tab-bar-tab-hints t)
-  :bind
-  (("M-h" . tab-bar-switch-to-prev-tab)
-   ("M-l" . tab-bar-switch-to-next-tab)
-   ("C-c t n" . tab-bar-new-tab)
-   ("C-c t c" . tab-bar-close-tab)))
 
 ;; Completion framework: Vertico and friends
 (use-package vertico
@@ -237,72 +202,6 @@
     (dolist (file my/exclude-files)
       (add-to-list 'treemacs-file-ignore-regexps (concat file "\\'")))))
 
-(use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'projectile
-    (require 'treemacs-projectile))
-  :bind
-  (("M-0" . treemacs-select-window)
-   ("C-c t" . treemacs))
-  :config
-  (setq treemacs-use-git-mode 'simple) ; 简化 Git 集成，减少依赖
-  (treemacs-follow-mode t)) ; 自动跟踪当前文件
-
-;; Configure treemacs-projectile
-(use-package treemacs-projectile
-  :ensure t
-  :after (treemacs projectile)
-  :demand t)
-
-(use-package treemacs
-  :ensure t
-  :defer t
-  :bind
-  (("M-0" . treemacs-select-window)
-   ("C-c t" . treemacs))
-  :config
-  (setq treemacs-use-git-mode 'simple)
-  (treemacs-follow-mode t))
-
-;; Project management
-(use-package projectile
-  :config
-  (projectile-mode +1)
-  :bind-keymap
-  ("C-c p" . projectile-command-map))
-(with-eval-after-load 'projectile
-  (add-to-list 'projectile-known-projects "~/.emacs.d"))
-
-;; Ensure projectile is installed and enabled
-(use-package projectile
-  :ensure t
-  :init
-  (projectile-mode +1)
-  :config
-  ;; Add directories to globally ignore
-  (dolist (dir '(".git"
-                 "__pycache__"
-                 "bazel-bin"
-                 "bazel-out"
-                 "bazel-testlogs"
-                 "bazel-*")) ; Wildcard for bazel-<workspace> symlinks
-    (add-to-list 'projectile-globally-ignored-directories dir))
-  ;; Add file patterns to globally ignore
-  (dolist (file '("*.pyc"
-                  "*.o"
-                  "*.a"
-                  "*.so"
-                  "bazel-*")) ; Ignore bazel-related files
-    (add-to-list 'projectile-globally-ignored-files file))
-  ;; Optional: Optimize indexing for large projects
-  (setq projectile-indexing-method 'hybrid)
-  ;; Auto-start Treemacs when switching projects
-  (add-hook 'projectile-after-switch-project-hook
-            (lambda ()
-              (treemacs-display-current-project-exclusively))))
-
 ;; 确保 undo-tree 安装
 (use-package undo-tree
   :ensure t
@@ -357,6 +256,75 @@
   (editorconfig-mode 1) ;; 全局启用
   ;; 确保 editorconfig 覆盖默认缩进设置
   (add-hook 'c++-mode-hook #'editorconfig-apply))
+
+;;; Projectile 配置
+(use-package projectile
+  :diminish projectile-mode ; 隐藏模式行中的 Projectile 标志
+  :config
+  (projectile-mode +1) ; 全局启用 Projectile
+  (setq projectile-project-search-path '("~/Projects/")) ; 设置项目根目录
+  (setq projectile-enable-caching t) ; 启用缓存以提高性能
+  (setq projectile-switch-project-action #'projectile-dired) ; 切换项目时打开 Dired
+  :bind-keymap
+  ("C-c p" . projectile-command-map) ; Projectile 命令前缀
+  :init
+  (when (file-directory-p "~/Projects")
+    (setq projectile-known-projects-file
+          (expand-file-name "~/.emacs.d/projectile-bookmarks.eld"))))
+
+;;; Treemacs 配置
+(use-package treemacs
+  :defer t
+  :config
+  (progn
+    (setq treemacs-collapse-dirs (if treemacs-python-executable 3 0) ; 折叠空目录
+          treemacs-file-event-delay 500 ; 降低文件事件延迟
+          treemacs-git-mode 'deferred ; 使用异步 Git 模式
+          treemacs-file-ignore-globs '("*.pyc" "node_modules" "__pycache__") ; 忽略特定文件
+          treemacs-sorting 'alphabetic-asc) ; 按字母排序
+    (treemacs-follow-mode t) ; 自动跟随当前缓冲区
+    (treemacs-filewatch-mode t) ; 启用文件系统监控
+    (treemacs-git-commit-diff-mode t)) ; 显示 Git 提交差异
+  :bind
+  (:map global-map
+        ("M-0" . treemacs-select-window) ; 切换到 Treemacs 窗口
+        ("C-x t t" . treemacs) ; 打开/切换 Treemacs
+        ("C-x t p" . treemacs-projectile)) ; 集成 Projectile
+  :hook
+  (treemacs-mode . (lambda () (display-line-numbers-mode -1)))) ; 禁用 Treemacs 中的行号
+
+;;; Treemacs 与 Projectile 集成
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+;;; Centaur Tabs 配置
+(use-package centaur-tabs
+  :demand
+  :config
+  (centaur-tabs-mode t)
+  (setq centaur-tabs-style "wave" ; 标签页样式
+        centaur-tabs-height 32 ; 标签高度
+        centaur-tabs-set-icons t ; 启用图标（需要 all-the-icons）
+        centaur-tabs-set-bar 'under ; 高亮条位置
+        centaur-tabs-set-modified-marker t ; 显示修改标记
+        centaur-tabs-cycle-scope 'tabs ; 标签循环范围
+        centaur-tabs-excluded-prefixes '("*" " *") ; 忽略特定缓冲区
+        centaur-tabs-group-by-projectile-project t) ; 按 Projectile 项目分组
+  (centaur-tabs-headline-match) ; 匹配标题样式
+  :bind
+  ("M-h" . centaur-tabs-backward) ; 上一标签
+  ("M-l" . centaur-tabs-forward) ; 下一标签
+  :hook
+  (dired-mode . centaur-tabs-local-mode) ; 在 Dired 中禁用标签
+  (treemacs-mode . centaur-tabs-local-mode)) ; 在 Treemacs 中禁用标签
+
+;;; 安装 all-the-icons 以支持 Centaur Tabs 和 Treemacs 的图标
+(use-package all-the-icons
+  :if (display-graphic-p)
+  :config
+  (unless (member "all-the-icons" (font-family-list))
+    (all-the-icons-install-fonts t)))
 
 (provide 'init)
 ;;; init.el ends here
